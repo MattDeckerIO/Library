4002f8918434ca3a1e0c8a491764cf457f02510f
Matt Decker <me@mattdecker.io>
2022-02-23 09:08:23 -0500

    Adds D9 settings.docksal.php

---
+++ Drupal 9/settings.docksal.php
@@ -0,0 +1,33 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => getenv('MYSQL_DATABASE'),
+  'username' => getenv('MYSQL_USER'),
+  'password' => getenv('MYSQL_PASSWORD'),
+  'host' => getenv('MYSQL_HOST'),
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
+
+$settings['hash_salt'] = '676b67d780d83cf9cbe6eab3ce76c80f6460368b';
+
+assert_options(ASSERT_ACTIVE, TRUE);
+\Drupal\Component\Assertion\Handle::register();
+
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['extension_discovery_scan_tests'] = FALSE;
+// $settings['rebuild_access'] = TRUE;
+// $settings['skip_permissions_hardening'] = TRUE;
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.logging']['error_level'] = 'verbose';
+
+$config['google_analytics.settings']['account'] = 'UA-0000000-00';
+
+$config['shield.settings']['credentials']['shield']['user'] = NULL;
