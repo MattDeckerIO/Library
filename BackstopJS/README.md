f7741dd2e5d178fb333c2958e80c1855ac0c6e91
Matt Decker <mdecker@air.org>
2024-05-16 10:39:47 -0400

    main Overhauls BackstopJS configuration and adds documentation.
    Adds default values.
    Adds environment-specific delay, capture limits, and cookies.
    Refactors string concatenation.

--- BackstopJS/README.md
+++ BackstopJS/README.md
@@ -9,3 +9,9 @@
 1. Run `backstop reference` to take reference screenshots
 1. Run `backstop test` to compare a test environment to the reference screenshots
 1. Open `backstop_data/html_report/index.html` to review diff.
+
+## Usage
+1. We recommend first reviewing the screenshots of a one-scenario test. e.g. `backstop test --filter=test --env=prd` to ensure BackstopJS can access the target environment.
+1. Run a reference job: e.g. `backstop reference --env=prd`
+1. Run a one-scenario test against the other environment. e.g. `backstop test --filter=test --env=dev` to ensure BackstopJS can access the target environment.
+1. Run a test job: `backstop test --env=dev`
--- BackstopJS/backstop.config.js
+++ BackstopJS/backstop.config.js
@@ -1,29 +1,55 @@
 // INSTALLATION:
-// backstop init
+// backstop init — This creates the necessary scripts.
 // Enter environment domains and scenarios.
 // Put cookie authentication data into /backstop_data/engine_scripts/cookies.json
 
 // USAGE:
-// backstop test --env=dev
+// backstop test --env=int --filter=test
 // backstop reference
 
+var defaultDelay        = 5000;
+var defaultCaptureLimit = 1;
+
 var envs = {
-  'lcl' : 'http://local.domain.com',
-  'dev' : 'https://user:pass@dev.domain.com',
-  'stg' : 'https://user:pass@stg.domain.com',
-  'prd' : 'https://www.domain.com'
+  'lcl' : {
+    'path': 'http://local.domain.com',
+    'cookies': 'lcl.cookies.json',
+    'delay' : 1,
+    'asyncCaptureLimit': 10,
+  },
+  'dev' : {
+    'path': 'https://user:pass@dev.domain.com',
+    'cookies': 'dev.cookies.json',
+  },
+  'stg' : {
+    'path': 'https://user:pass@stg.domain.com',
+    'cookies': 'stg.cookies.json',
+  },
+  'prd' : {
+    'path': 'https://www.domain.com',
+    'cookies': 'prd.cookies.json',
+  },
 }
 
-var env = 'prd';
 var argv = process.argv.slice(2);
-var testURL = envs[env];
-
 argv.forEach(function (e) {
   var dat = e.split('=');
   if (dat.length == 2 && dat[0] == '--env') { env = dat[1]; }
 });
 
-if (env && envs[env] !== undefined) { testURL = envs[env] }
+var env = env ?? 'prd';
+var path = String(envs[env]['path']);
+var delay = envs[env]['delay'] ?? defaultDelay;
+var asyncCaptureLimit = envs[env]['asyncCaptureLimit'] ?? defaultCaptureLimit;
+var cookiePath = String(envs[env]['cookies']);
+
+console.log('');
+console.log('--')
+console.log('Analyzing domain:',path);
+console.log('Capturing',asyncCaptureLimit,'screen','per',delay,'ms');
+console.log('With cookie file:',cookiePath);
+console.log('--')
+console.log('');
 
 module.exports = {
   "id": "backstop_default",
@@ -31,37 +57,46 @@ module.exports = {
     {
       "label": "phone",
       "width": 360,
-      "height": 640
+      "height": 800,
+      "source" : "https://gs.statcounter.com/screen-resolution-stats/mobile/worldwide",
     },
     {
       "label": "tablet",
       "width": 768,
-      "height": 1024
+      "height": 1024,
+      "source": "https://gs.statcounter.com/screen-resolution-stats/tablet/worldwide",
     },
     {
       "label": "desktop_large",
       "width": 1920,
-      "height": 1080
+      "height": 1080,
+      "source": "https://gs.statcounter.com/screen-resolution-stats/desktop/worldwide",
     }
   ],
   "scenarioDefaults": {
-    // Remove the element and retain the space.
     "hideSelectors": [
       ""
     ],
-    // Remove the element and retain the space.
     "removeSelectors": [
-      "svg.ext",
+      ""
     ],
-    "delay": 5000,
-    "cookiePath": "backstop_data/cookies/env.project.json",
+    "delay": delay,
+    "requireSameDimensions": false,
+    "cookiePath": 'backstop_data/cookies/'+cookiePath,
     "onBeforeScript": "puppet/onBefore.js",
+    "onReadyScript": "puppet/onReady.js",
   },
   "scenarios": [
     {
-      "label": "Home",
-      "url": `${testURL}/`,
-      "referenceUrl": 'https://www.domain.com',
+      'label': 'UserAgent',
+      'url': 'https://51degrees.com/developers/user-agent-tester',
+      'referenceUrl': 'https://51degrees.com/developers/user-agent-tester',
+      'delay': 0,
+    },
+    {
+      'label': 'Homepage test',
+      'url': path+'/',
+      'referenceUrl': path+'/',
     },
   ],
   "paths": {
@@ -76,7 +111,7 @@ module.exports = {
   "engineOptions": {
     "args": ["--no-sandbox"]
   },
-  "asyncCaptureLimit": 1,
+  "asyncCaptureLimit": asyncCaptureLimit,
   "asyncCompareLimit": 10,
   "debug": false,
   "debugWindow": false
